bc2c8742db1fb51917bbb2464201a176d565d763
brookecarbine-ui <brookecarbine@gmail.com>
2025-11-09 16:56:16 -0700

    Add files via upload

--- index.js
+++
@@ -1,5 +0,0 @@
-const greeting= document.getElementById("greeting");
-greeting.style.textAlign= "center";
-greeting.style.marginTop= "50px";
-const username= window.prompt("How can I help today?", "Find Necklaces");
-greeting.textContent= "Welcome to Petal and Chain, " + username + "!";
